77b5a0e4ed31f1584e16f2f0e101c1633e0093aa
VisheshBansal <vishesh.bansal@gmail.com>
2021-05-05 20:22:39 +0530

    feat: added ocr support

--- routes/ocr.js
+++ routes/ocr.js
@@ -4,10 +4,18 @@ var uploads = multer({dest: '../uploads'});
 const shell = require("shelljs");
 require("dotenv").config();
 var fs = require('fs-extra');
+const ocrSpaceApi = require('ocr-space-api');
+
+var options =  {
+    apikey: process.env.API_KEY,
+    language: 'eng', // English
+    imageFormat: 'image/png', // Image Type (Only png ou gif is acceptable at the moment i wrote this)
+    isOverlayRequired: true
+};
+
 
 const ocrSpace = require('ocr-space-api-wrapper')
 router.post('/', uploads.single('file'), (req, res, next) => {
-    //  console.log(req.file);
     if (req.file === undefined) {
         var err = new Error('error');
         next(err);
@@ -16,6 +24,13 @@ router.post('/', uploads.single('file'), (req, res, next) => {
 
     let filepath = "uploads/temp";
 
+// Run and wait the result
+    ocrSpaceApi.parseImageFromLocalFile(filepath, options)
+        .then(function (parsedResult) {
+            console.log(parsedResult.parsedText);
+        }).catch(function (err) {
+        console.log('ERROR:', err);
+    });
 
 })
 
